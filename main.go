3ce1852f24901135bfb273caadccbf3727f40e78
A1esandr <ravulmirt@gmail.com>
2020-04-25 20:50:47 +0500

    Added buttons

--- main.go
+++ main.go
@@ -10,23 +10,46 @@ import (
 func main() {
 	botToken := os.Getenv("BOT_TOKEN")
 	if len(botToken) == 0 {
-		panic("Bot token not set!")
+		log.Fatalf("Bot token not set!")
 	}
 
-	bot, err := botgolang.NewBot(botToken)
+	bot, err := botgolang.NewBot(botToken, botgolang.BotDebug(true))
 	if err != nil {
-		log.Println("wrong token")
+		log.Fatalf("Сannot connect to bot: %s", err)
 	}
+
+	log.Println(bot.Info)
 
 	ctx, _ := context.WithCancel(context.Background())
 	updates := bot.GetUpdatesChannel(ctx)
 	for update := range updates {
-		// your awesome logic here
-		chatID := update.Payload.Chat.ID
-		message := bot.NewTextMessage(chatID, "Hi, "+update.Payload.From.FirstName)
-		err = message.Send()
-		if err != nil {
-			log.Println(err.Error())
+
+		log.Println(update.Type, update.Payload)
+		switch update.Type {
+		case botgolang.NEW_MESSAGE:
+			message := update.Payload.Message()
+
+			helloBtn := botgolang.NewCallbackButton("Hello", "echo")
+			goBtn := botgolang.NewURLButton("go", "https://golang.org/")
+			message.AttachInlineKeyboard([][]botgolang.Button{{helloBtn, goBtn}})
+
+			if err := message.Send(); err != nil {
+				log.Printf("failed to send message: %s", err)
+			}
+		case botgolang.EDITED_MESSAGE:
+			message := update.Payload.Message()
+			if err := message.Reply("do not edit!"); err != nil {
+				log.Printf("failed to reply to message: %s", err)
+			}
+		case botgolang.CALLBACK_QUERY:
+			data := update.Payload.CallbackQuery()
+			switch data.CallbackData {
+			case "echo":
+				response := bot.NewButtonResponse(data.QueryID, "", "Hello World!", false)
+				if err := response.Send(); err != nil {
+					log.Printf("failed to reply on button click: %s", err)
+				}
+			}
 		}
 	}
 
